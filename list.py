f0bb270257fd65b4ee67bbd436a5fee90acbecd0
glohar1993 <125760763+glohar1993@users.noreply.github.com>
2023-11-17 16:01:00 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,13 +1,13 @@
 import json
 import os
-from datetime import datetime
 
 html_table = """
-<table>
+<html>
+<body>
+<table border='1'>
 <tr>
     <th>Parameter Name</th>
     <th>File Name</th>
-    <th>Creation Date</th>
 </tr>
 """
 
@@ -16,10 +16,15 @@ for filename in os.listdir('s3_contents'):
         with open(f's3_contents/{filename}', 'r') as file:
             data = json.load(file)
             param_name = data.get('Name', 'N/A')
-            creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Replace with actual creation date if available
-            html_table += f"<tr><td>{param_name}</td><td>{filename}</td><td>{creation_date}</td></tr>"
+            html_table += f"<tr><td>{param_name}</td><td>{filename}</td></tr>"
 
-html_table += "</table>"
+html_table += """
+</table>
+</body>
+</html>
+"""
 
 with open("table.html", "w") as file:
     file.write(html_table)
+
+print("HTML table generated.")
